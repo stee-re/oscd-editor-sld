11a884b8fce63679af5f32cbe931939a5e650f5c
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-01-22 08:19:49 +0100

    fix: deployable pre-coord-refactor-version

--- rollup.config.js
+++ rollup.config.js
@@ -1,78 +1,69 @@
-import path from 'path';
+import fs from 'fs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
-// import { copy } from '@web/rollup-plugin-copy';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import { terser } from 'rollup-plugin-terser';
-import { generateSW } from 'rollup-plugin-workbox';
+import copy from 'rollup-plugin-copy';
 
-
-export default {
-  input: 'oscd-editor-sld.ts',
-  output: {
-    sourcemap: true,
-    format: 'es',
-    dir: 'dist',
-  },
-
-  plugins: [
-    typescript(),
-    /** Resolve bare module imports */
-    nodeResolve(),
-    /** Minify JS */
-    terser(),
-    /** Bundle assets references via import.meta.url */
-    importMetaAssets(),
-    /** Compile JS to a lower language target */
-    babel({
-      babelHelpers: 'bundled',
-      presets: [
-        [
-          require.resolve('@babel/preset-env'),
-          {
-            targets: [
-              'last 3 Chrome major versions',
-              'last 3 Firefox major versions',
-              'last 3 Edge major versions',
-              'last 3 Safari major versions',
-            ],
-            modules: false,
-            bugfixes: true,
-          },
-        ],
-      ],
-      plugins: [
-        [
-          require.resolve('babel-plugin-template-html-minifier'),
-          {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
-            failOnError: false,
-            strictCSS: true,
-            htmlMinifier: {
-              collapseWhitespace: true,
-              conservativeCollapse: true,
-              removeComments: true,
-              caseSensitive: true,
-              minifyCSS: true,
-            },
-          },
-        ],
-      ],
-    }),
-    /** Create and inject a service worker */
-    generateSW({
-      globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
-      navigateFallback: '/index.html',
-      // where to output the generated sw
-      swDest: path.join('dist', 'sw.js'),
-      // directory to match patterns against to be precached
-      globDirectory: path.join('dist'),
-      // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
-      skipWaiting: true,
-      clientsClaim: true,
-      runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
-    }),
-  ],
+const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8'));
+const demoTsconfig = {
+  ...tsconfig,
+  compilerOptions: { ...tsconfig.compilerOptions, outDir: 'dist/demo' },
 };
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+const name = packageJson.name.split('/').pop();
+
+export default [
+  {
+    input: `${name}.ts`,
+    output: {
+      sourcemap: true, // Add source map to build output
+      format: 'es', // ES module type export
+      dir: 'dist', // The build output folder
+      // preserveModules: true, // Keep directory structure and files
+    },
+    preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
+
+    plugins: [
+      /** Resolve bare module imports */
+      nodeResolve(),
+
+      typescript(),
+
+      /** Bundle assets references via import.meta.url */
+      importMetaAssets(),
+    ],
+  },
+  {
+    input: 'demo/index.html',
+    plugins: [
+      html({
+        input: 'demo/index.html',
+        minify: true,
+      }),
+      /** Resolve bare module imports */
+      nodeResolve(),
+
+      typescript(demoTsconfig),
+
+      /** Bundle assets references via import.meta.url */
+      importMetaAssets(),
+      copy({
+        targets: [
+          { src: 'demo/sample.scd', dest: 'dist/demo' },
+          { src: 'demo/*.js', dest: 'dist/demo' },
+          // Add more patterns if you have more assets
+        ],
+        verbose: true,
+        flatten: false,
+      }),
+    ],
+    output: {
+      dir: 'dist/demo',
+      format: 'es',
+      sourcemap: true,
+    },
+  },
+];
