5522e6a9aba90c98b3dfb33878b9c83923ebe442
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-05-08 09:45:34 +0200

    chore: refactor IED ref helpers & sld attr helpers into foundations

---
+++ src/foundations/sld-attributes.ts
@@ -0,0 +1,133 @@
+import { getReference } from '@openscd/scl-lib';
+
+import { isIedReferenceElement } from './ied.js';
+import { privType, sldNs } from './namespaces.js';
+import { isTransformerKind } from './transformer.js';
+
+import type { EditV2 } from '@openscd/oscd-api';
+import type { Attrs, Point } from './types.js';
+
+export function xmlBoolean(value?: string | null) {
+  return ['true', '1'].includes(value?.trim() ?? 'false');
+}
+
+function sldAttributes(element: Element, nsPrefix?: string): Element | null {
+  if (isIedReferenceElement(element)) {
+    const referenceSldAttrs = element.querySelector(':scope > SLDAttributes');
+    if (referenceSldAttrs) {
+      return referenceSldAttrs;
+    }
+    if (!nsPrefix) {
+      return null;
+    }
+
+    const sldAttrs = element.ownerDocument.createElementNS(
+      sldNs,
+      `${nsPrefix}:SLDAttributes`,
+    );
+    element.appendChild(sldAttrs);
+    return sldAttrs;
+  }
+
+  const sldAttrs = element.querySelector(
+    `:scope > Private[type="${privType}"] > SLDAttributes`,
+  );
+
+  if (sldAttrs) {
+    return sldAttrs;
+  }
+  if (!nsPrefix) {
+    return null;
+  }
+
+  const doc = element.ownerDocument;
+  const priv = doc.createElementNS(doc.documentElement.namespaceURI, 'Private');
+
+  priv.setAttribute('type', privType);
+  element.insertBefore(priv, getReference(element, 'Private'));
+
+  const sldAttrsNew = doc.createElementNS(sldNs, `${nsPrefix}:SLDAttributes`);
+  priv.insertBefore(sldAttrsNew, null);
+
+  return sldAttrsNew;
+}
+
+export function setSLDAttributes(
+  element: Element,
+  nsPrefix: string,
+  values: Record<string, string>,
+): void {
+  const isSectionOrVertex = ['Section', 'Vertex'].includes(element.localName);
+
+  if (isSectionOrVertex) {
+    Object.entries(values).forEach(([key, value]) => {
+      element.setAttributeNS(sldNs, `${nsPrefix}:${key}`, value);
+    });
+  } else {
+    Object.entries(values).forEach(([key, value]) =>
+      sldAttributes(element, nsPrefix)?.setAttributeNS(
+        sldNs,
+        `${nsPrefix}:${key}`,
+        value,
+      ),
+    );
+  }
+}
+
+export function updateSLDAttributes(
+  element: Element,
+  nsPrefix: string,
+  values: Partial<Record<string, string | null>>,
+): EditV2 {
+  const isSectionOrVertex = ['Section', 'Vertex'].includes(element.localName);
+  const toBeUpdated = isSectionOrVertex
+    ? element
+    : sldAttributes(element, nsPrefix)!;
+
+  return {
+    element: toBeUpdated,
+    attributesNS: {
+      [sldNs]: Object.fromEntries(
+        Object.entries(values).map(([key, value]) => [
+          `${nsPrefix}:${key}`,
+          value,
+        ]),
+      ),
+    },
+  };
+}
+
+export function getSLDAttributes(element: Element, key: string): string | null {
+  const isSectionOrVertex = ['Section', 'Vertex'].includes(element.localName);
+  if (isSectionOrVertex) {
+    return element.getAttributeNS(sldNs, key);
+  }
+
+  return sldAttributes(element)?.getAttributeNS(sldNs, key) ?? null;
+}
+
+export function attributes(element: Element): Attrs {
+  const [x, y, w, h, rotVal, labelX, labelY] = [
+    'x',
+    'y',
+    'w',
+    'h',
+    'rot',
+    'lx',
+    'ly',
+  ].map(name => parseFloat(getSLDAttributes(element, name) ?? '0'));
+  const weight = parseInt(getSLDAttributes(element, 'weight') ?? '300', 10);
+  const pos = [x, y].map(d => Math.max(0, d)) as Point;
+  const dim = [w, h].map(d => Math.max(1, d)) as Point;
+  const label = [labelX, labelY].map(d => Math.max(0, d)) as Point;
+
+  const bus = xmlBoolean(getSLDAttributes(element, 'bus'));
+  const flip = xmlBoolean(getSLDAttributes(element, 'flip'));
+  const kindVal = getSLDAttributes(element, 'kind');
+  const kind = isTransformerKind(kindVal) ? kindVal : 'default';
+  const color = getSLDAttributes(element, 'color') || '#000';
+
+  const rot = (((rotVal % 4) + 4) % 4) as 0 | 1 | 2 | 3;
+
+  return { pos, dim, label, flip, rot, bus, weight, color, kind };
+}
